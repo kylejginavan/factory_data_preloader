1ef954668256d162d5eee45c7184a084fc87b253
Myron Marston <myron.marston@gmail.com>
2009-03-30 10:45:57 -0700

    Make this gem work with Rails 2.2 and 2.3: - monkey patch either Test::Unit::TestCase or ActiveRecord::TestFixtures, based on if ActiveRecord::TestFixtures is defined (for 2.3) - monkey patch Object with #try method unless defined (defined in 2.3, not in 2.2) - load the appropriate rails version based on the RAILS_VERSION environment variable.  This lets you test a specific version using "rake test RAILS_VERSION=x.x.x".

--- lib/factory_data_preloader.rb
+++ lib/factory_data_preloader.rb
@@ -1,8 +1,16 @@
 require 'test/unit'
 require 'rubygems'
+
+if ENV['RAILS_VERSION']
+  puts "loading Rails version #{ENV['RAILS_VERSION']}"
+  gem "activesupport", "= #{ENV['RAILS_VERSION']}"
+  gem "activerecord", "= #{ENV['RAILS_VERSION']}"
+end
+
 require 'active_support'
 require 'active_record'
 require 'active_record/fixtures'
 
+require 'factory_data_preloader/core_ext'
 require 'factory_data_preloader/factory_data'
 require 'factory_data_preloader/rails_core_ext'
--- lib/factory_data_preloader/rails_core_ext.rb
+++ lib/factory_data_preloader/rails_core_ext.rb
@@ -1,26 +1,29 @@
-# TODO: figure out why this doesn't work...
+# Between Rails 2.2 and 2.3, the fixture loading code was moved from
+# Test::Unit::TestCase to ActiveRecord::TestFixtures.  See this commit:
+# http://github.com/rails/rails/commit/b0ee1bdf2650d7a8380d4e9be58bba8d9c5bd40e
+patch_module = defined?(ActiveRecord::TestFixtures) ? ActiveRecord::TestFixtures : Test::Unit::TestCase
 
-# class Test::Unit::TestCase
-#   def load_fixtures_with_preloaded_factory_data
-#     val = load_fixtures_without_preloaded_factory_data
-#     FactoryData.preload_data!
-#     val
-#   end
-#     
-#   def teardown_fixtures_with_preloaded_factory_data
-#     FactoryData.reset_cache
-#     teardown_fixtures_without_preloaded_factory_data
-#   end
-# 
-#   alias_method_chain :load_fixtures, :preloaded_factory_data
-#   alias_method_chain :teardown_fixtures, :preloaded_factory_data
-# end
-# 
-# class Fixtures
-#   def delete_existing_fixtures_with_preloaded_factory_data
-#     delete_existing_fixtures_without_preloaded_factory_data
-#     FactoryData.delete_preload_data!
-#   end
-#   
-#   alias_method_chain :delete_existing_fixtures, :preloaded_factory_data
-# end
+patch_module.class_eval do
+  def load_fixtures_with_preloaded_factory_data
+    val = load_fixtures_without_preloaded_factory_data
+    FactoryData.preload_data!
+    val
+  end
+    
+  def teardown_fixtures_with_preloaded_factory_data
+    FactoryData.reset_cache!
+    teardown_fixtures_without_preloaded_factory_data
+  end
+
+  alias_method_chain :load_fixtures, :preloaded_factory_data
+  alias_method_chain :teardown_fixtures, :preloaded_factory_data
+end
+
+class Fixtures
+  def delete_existing_fixtures_with_preloaded_factory_data
+    delete_existing_fixtures_without_preloaded_factory_data
+    FactoryData.delete_preload_data!
+  end
+  
+  alias_method_chain :delete_existing_fixtures, :preloaded_factory_data
+end
